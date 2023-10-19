cac1570716e2e18454fc6b1203f459b479984788
Julian Waller <git@julusian.co.uk>
2023-10-19 17:02:58 +0100

    chore: update deployment branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/casparcg/casparcg.github.io/tree/docusaurus/",
+            "https://github.com/casparcg/casparcg.github.io/tree/main/",
         },
         blog: {
           showReadingTime: true,
